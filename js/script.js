42ea36ab644419c63b7f1eb92c167e0cd322d113
Daniel Nowrot <daniel.nowrot@gmail.com>
2022-12-05 12:59:29 +0100

    rebuils js code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-console.log("Cześć! To moja pierwsza strona");
+{
+    const butt1 = document.querySelector(".js-button");
+    
+    const welcome = () => {
+        console.log("Cześć! To moja pierwsza strona");
+    }
 
-let butt1 = document.querySelector(".js-button");
-let themeBody = document.querySelector(".js-body");
-let themeButton = document.querySelector(".js-button");
-let buttonName = document.querySelector(".js-button__name");
+    let onChangeBackgroundClick = () => {
+        const themeBody = document.querySelector(".js-body");
+        const themeButton = document.querySelector(".js-button");
+        const buttonName = document.querySelector(".js-button__name");
 
-butt1.addEventListener("click", () => {
-    themeBody.classList.toggle("body--dark");
-    themeButton.classList.toggle("button--dark");
+        themeBody.classList.toggle("body--dark");
+        themeButton.classList.toggle("button--dark");
 
-    buttonName.innerText = themeBody.classList.contains("body--dark") ?
-        "Jasny" : "Ciemny";
-})
+        buttonName.innerText = themeBody.classList.contains("body--dark") ?
+            "Jasny" : "Ciemny";
+    }
 
-let tableThemes = document.querySelectorAll(".table__cell");
-let navElements = document.querySelectorAll(".navigation__element");
+    let changeElementsTheme = () => {
+        const tableThemes = document.querySelectorAll(".table__cell");
+        const navElements = document.querySelectorAll(".navigation__element");
+       
+        tableThemes.forEach((tableCell) => {
+            butt1.addEventListener("click", () => {
+                tableCell.classList.toggle("table__cell--dark");
+            })
+        })
+        navElements.forEach((navElement) => {
+            butt1.addEventListener("click", () => {
+                navElement.classList.toggle("navigation__element--dark");
+            })
+        })
+    }
 
-tableThemes.forEach((tableCell) => {
-    butt1.addEventListener("click", () => {
-        tableCell.classList.toggle("table__cell--dark");
-    })
-})
-navElements.forEach((navElement) => {
-    butt1.addEventListener("click", () => {
-        navElement.classList.toggle("navigation__element--dark");
-    })
-})
+    welcome();
+    butt1.addEventListener("click", onChangeBackgroundClick);
+    changeElementsTheme();
+}
